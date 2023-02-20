bdf0ad8bce6a191e7835485039fa44bb87d11284
Ido Ben-Hur <idoybh2@gmail.com>
2023-02-20 06:58:34 +0100

    Settings: Allow scheduling always on display [2/2]
    * Squash of
      yaap/packages_apps_YASP@30441ec
      yaap/packages_apps_YASP@b896aa6
      yaap/packages_apps_YASP@f500a60
    
    [jhonboy121]: adapted to A13 and use a preference controller to update the preference
    
    Co-Authored-By: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: If736f5609ca660e3c9797a309c814d2dcd618d31

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -37,6 +37,7 @@ import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
+import com.android.settings.display.AODSchedulePreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
@@ -55,7 +56,6 @@ import java.util.List;
 @SearchIndexable
 public class LockscreenDashboardFragment extends DashboardFragment
         implements OwnerInfoPreferenceController.OwnerInfoCallback {
-
     private static final String TAG = "LockscreenDashboardFragment";
 
     @VisibleForTesting
@@ -115,6 +115,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
+        use(AODSchedulePreferenceController.class).setConfig(getConfig(context));
 
         mControlsContentObserver = new ContentObserver(
                 new Handler(Looper.getMainLooper())) {
